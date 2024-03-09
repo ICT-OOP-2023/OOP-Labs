59ef3f7257a11b74a5c52ea2f51076d3327b9389
SkylarPro <skylarpro@MacBook-Pro-SkylarPro.local>
2024-03-09 11:44:34 +0300

    First lab work

--- OOP-ICT.First/Models/Dealer.cs
+++ OOP-ICT.First/Models/Dealer.cs
@@ -2,5 +2,22 @@
 
 public class Dealer
 {
-    
+    public Dealer()
+    {
+        Deck = new Deck();
+        Deck.Shuffle();
+    }
+
+    public Deck Deck { get; }
+
+    public IReadOnlyCollection<Card> DealCards(int count)
+    {
+        var dealtCards = new List<Card>();
+        for (var i = 0; i < count; i++)
+        {
+            dealtCards.Add(Deck.DrawCard());
+        }
+
+        return dealtCards;
+    }
 }
--- OOP-ICT.First/Program.cs
+++ OOP-ICT.First/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using OOP_ICT.Models;
+
+namespace OOP_ICT;
+
+internal abstract class Program
+{
+    private static void Main()
+    {
+        var dealer = new Dealer();
+        
+        var hand = dealer.DealCards(5);
+
+        foreach (var card in hand)
+        {
+            Console.WriteLine($"{card.Rank} of {card.Suit}");
+        }
+    }
+}
